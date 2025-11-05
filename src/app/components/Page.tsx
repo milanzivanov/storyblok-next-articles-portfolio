27744d20c61f2e24244c9842a6485c2a95ae9ea9
milan <milanzivanov@users.noreply.github.com>
2025-11-05 14:58:36 +0100

    clening

--- src/app/components/Page.tsx
+++ src/app/components/Page.tsx
@@ -1,5 +1,3 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { StoryblokHomeContent } from "@/src/interface";
 import { SbBlokData, storyblokEditable, StoryblokServerComponent } from "@storyblok/react/rsc";
 
 interface PageProps {
